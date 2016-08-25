433f89c62680b64a9035ab4eb0bf0eb2d1b0fd12
zhengjiong <zhengjiong@zhengjiongdeMacBook-Pro.local>
2016-08-25 16:40:29 +0800

    add demo4

---
+++ app/src/main/java/com/zj/example/clip/ClipChildrenDemo4Activity.java
@@ -0,0 +1,24 @@
+package com.zj.example.clip;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Title: ClipChildrenDemo4Activity
+ * Description:
+ * Copyright:Copyright(c)2016
+ * Company: 博智维讯信息技术有限公司
+ * CreateTime:16/8/25  16:24
+ *
+ * @author 郑炯
+ * @version 1.0
+ */
+public class ClipChildrenDemo4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
